2b94b45dc2ba9f30e8205da3ce3ebbaeb3d07cf9
EnyaChang2002 <yyy2011taiwan@gmail.com>
2025-11-16 21:41:23 +0800

    temporary work

---
+++ week03/scripts/place.js
@@ -0,0 +1,12 @@
+document.getElementById("last-modified").innerHTML = document.lastModified;
+document.getElementById("current-year").innerHTML = new Date().getFullYear();
+
+// const api_key = "5446ec4d0fdf6b5d3f6cd10cec07f0a8";
+
+//總之就是＋<li>Temperature: </li>
+//     <li>Condition: </li>
+//   <li>Wind: </li>
+// <li>Wind Chill: </li>
+//然後再加上把馬達加斯加複製上去，貓的圖片找網址縮小
+//接下來去做c#的作業
+//然後去把institute讀完
